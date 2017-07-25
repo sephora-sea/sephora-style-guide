a626ed06947de9115c5d3a4b50df0043d862f602
Eric Ewe Yow Choong <eric.ewe@luxola.com>
2017-07-25 14:57:12 +0800

    [#140641593] Complete carousel and touch up on css

---
+++ app/assets/javascripts/sephora_style_guide/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.product-card').click(function(e) {
+    if (e.target.className === 'carousel-arrow-right') {
+      $('.product-card-variants').animate({ scrollLeft: '+=60' }, 500);
+      $('.carousel-more-variants').hide();
+    } else if (e.target.className === 'carousel-arrow-left') {
+      $('.product-card-variants').animate({ scrollLeft: '-=60' }, 500);
+      $('.carousel-more-variants').show();
+    }
+  });
+})
