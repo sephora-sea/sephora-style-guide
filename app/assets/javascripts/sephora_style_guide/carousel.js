9fe9560e1b44bec32dcfbeb2559b1b19112270ad
Eric Ewe Yow Choong <eric.ewe@luxola.com>
2017-07-27 17:08:51 +0800

    [#140641593] Enable variant carousel scrollTo

--- app/assets/javascripts/sephora_style_guide/carousel.js
+++ app/assets/javascripts/sephora_style_guide/carousel.js
@@ -8,4 +8,17 @@ $(document).ready(function() {
       $('.carousel-more-variants').show();
     }
   });
+
+  $('.product-card').mouseover(function(e) {
+    if ($('[name="variant11"]').hasClass('active')) {
+      $('.product-card-variants').animate({ scrollLeft: $('[name="variant11"]').offset().left }, 500);
+      $('.carousel-more-variants').hide();
+    } else if ($('[name="variant12"]').hasClass('active')) {
+      $('.product-card-variants').animate({ scrollLeft: $('[name="variant12"]').offset().left }, 500);
+      $('.carousel-more-variants').hide();
+    } else if ($('[name="variant13"]').hasClass('active')) {
+      $('.product-card-variants').animate({ scrollLeft: $('[name="variant13"]').offset().left }, 500);
+      $('.carousel-more-variants').hide();
+    }
+  });
 });
