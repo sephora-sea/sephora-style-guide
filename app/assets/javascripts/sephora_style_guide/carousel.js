68c7e2d6f2cfa0eb5a902d085c7ec792b810da2e
Eric Ewe <ycewe@users.noreply.github.com>
2017-07-31 14:34:50 +0800

    [#140641593] Styleguide: product Card (#11)
    * [#140641593] Add base Product Card
    
    * [#140641593] Change product display size
    
    * [#140641593] Add mobile breakpoint
    
    * [#140641593] Fix size on mobile
    
    * [#140641593] Change markup layout
    
    * [#140641593] Correct device sizes
    
    * [#140641593] Enable responsiveness
    
    * [#140641593] Adjust font and image size
    
    * [#140641593] Add images to product card
    
    * [#140641593] Finalize product card styling
    
    * [#140641593] Fix hound violation
    
    * [#140641593] Add image sprite and remove redundant images
    
    * [#140641593] Clean variable name and fix hover
    
    * [#140641593] Fix hound violations
    
    * [#140641593] Remove redundant constants
    
    * [#140641593] Add grid layout
    
    * [#140641593] Fix image sizes and add third product
    
    * [#140641593] Display actions only when hover
    
    * [#140641593] Add base variant and ratings logic
    
    * [#140641593] Complete carousel and touch up on css
    
    * [#140641593] Fix hound violations
    
    * [#140641593] Push wishlist heart logic to view side
    
    * [#140641593] Fix wishlist heart alignment
    
    * [#140641593] Fix hound violation
    
    * [#140641593] Fix product label alignment
    
    * [#140641593] Fix heart hover colour and size
    
    * [#140641593] Enable variant carousel scrollTo
    
    * [#140641593] Remove redundant parameter
    
    * [#140641593] Fix variant selection on third product
    
    * [#140641593] Set default variant
    
    * [#140641593] Clean carousel code
    
    * [#140641593] Fix syntax error
    
    * [#140641593] Fix variant deselect bug
    
    * [#140641593] Fix heart display on mobile and tablet
    
    * [#140641593] Fix OOS text alignment

---
+++ app/assets/javascripts/sephora_style_guide/carousel.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('.product-card').click(function(e) {
+    if (e.target.className === 'carousel-arrow-right') {
+      $('.product-card-variants').animate({ scrollLeft: '+=60' }, 500);
+      $('.carousel-more-variants').hide();
+    } else if (e.target.className === 'carousel-arrow-left') {
+      $('.product-card-variants').animate({ scrollLeft: '-=60' }, 500);
+      $('.carousel-more-variants').show();
+    }
+  });
+
+  $('.product-card').mouseover(function() {
+    if ($('[name="variant11"]').hasClass('active')) {
+      $('.product-card-variants').animate({ scrollLeft: $('[name="variant11"]').offset().left }, 500);
+      $('.carousel-more-variants').hide();
+    } else if ($('[name="variant12"]').hasClass('active')) {
+      $('.product-card-variants').animate({ scrollLeft: $('[name="variant12"]').offset().left }, 500);
+      $('.carousel-more-variants').hide();
+    } else if ($('[name="variant13"]').hasClass('active')) {
+      $('.product-card-variants').animate({ scrollLeft: $('[name="variant13"]').offset().left }, 500);
+      $('.carousel-more-variants').hide();
+    }
+  });
+});
