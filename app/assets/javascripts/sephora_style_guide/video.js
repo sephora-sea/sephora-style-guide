8942931f285407284eb64a2502fedbf0740279ab
Eric Ewe <eric.ewe@sephoradigital.com>
2017-08-04 18:26:15 +0800

    [#140641937] Add the rest of video components

---
+++ app/assets/javascripts/sephora_style_guide/video.js
@@ -0,0 +1,49 @@
+// https://developers.google.com/youtube/iframe_api_reference
+
+$(document).ready(function() {
+  // Inject YouTube API script
+  var tag = document.createElement('script');
+  tag.src = "//www.youtube.com/player_api";
+  var firstScriptTag = document.body.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
+
+// global variable for the players
+var players = [];
+
+// this function gets called when API is ready to use
+function onYouTubeIframeAPIReady() {
+  var allPlayers = $('.modal-video');
+
+  for (var el in Array.from(allPlayers)) {
+    var player = new YT.Player($(allPlayers[el]).attr('id'), {
+      events: {
+        'onReady': onPlayerReady
+      }
+    });
+    players.push(player);
+  }
+}
+
+function onPlayerReady(event) {
+  // bind events
+  $('.close').click(function() {
+    var currentVideoId = $(this).closest('.modal-header').siblings().find('.modal-video').attr('id');
+
+    for (var i in players) {
+      if (players[i].a.id === currentVideoId) {
+        players[i].pauseVideo();
+      }
+    }
+  });
+
+  $('.video-link').click(function() {
+    var currentVideoId = $(this).attr('href').replace(/#|-/g, '');
+
+    for (var i in players) {
+      if (players[i].a.id.toLowerCase() === currentVideoId) {
+        players[i].playVideo();
+      }
+    }
+  });
+}
