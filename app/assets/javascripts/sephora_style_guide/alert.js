f76f7d87107d5ef6aaf1d786d3abf762383dab3e
Eric Ewe Yow Choong <eric.ewe@luxola.com>
2017-10-17 16:20:53 +0800

    Fix hound issues

--- app/assets/javascripts/sephora_style_guide/alert.js
+++ app/assets/javascripts/sephora_style_guide/alert.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   function alert() {
     var PARENT_CLASS = '.alert[role="alert"]',
-        CLOSE_BTN = '.alert[role="alert"] .close[data-dismiss="alert"]'
+        CLOSE_BTN = '.alert[role="alert"] .close[data-dismiss="alert"]';
 
-    $(document).on('click', CLOSE_BTN, function (e) {
-      $(this).parent(PARENT_CLASS).fadeOut(200)
+    $(document).on('click', CLOSE_BTN, function () {
+      $(this).parent(PARENT_CLASS).fadeOut(200);
     });
   }
 
