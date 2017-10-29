13b193ab0a0f8a3c794b5a1f92aaadbbabdad87e
Justin Ho <zamakkat@gmail.com>
2017-10-29 13:03:25 +0800

    Clean up controllers and helpers

--- app/controllers/sephora_style_guide/home_controller.rb
+++ app/controllers/sephora_style_guide/home_controller.rb
@@ -1,34 +1,6 @@
 module SephoraStyleGuide
   class HomeController < SephoraStyleGuide::ApplicationController
     def index
-      @buttons = buttons
-    end
-
-    private
-
-    def buttons
-      [
-        {
-          name: 'Default',
-          class: nil
-        },
-        {
-          name: 'Primary',
-          class: 'btn-primary'
-        },
-        {
-          name: 'White',
-          class: 'btn-white'
-        },
-        {
-          name: 'Grey',
-          class: 'btn-grey'
-        },
-        {
-          name: 'Disabled',
-          class: 'disabled'
-        }
-      ].freeze
     end
   end
 end
