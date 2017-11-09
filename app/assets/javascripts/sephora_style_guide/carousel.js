de34e19e047aa69c6b1f9c73dda5508fbb13a6f9
Eric Ewe <ycewe@users.noreply.github.com>
2017-11-09 14:34:34 +0800

    Extract reusable components (#49)
    * Make product related components resuable
    
    * Add select component
    
    * Add mobile support for oos icon
    
    * Remove important declarations

--- app/assets/javascripts/sephora_style_guide/carousel.js
+++ app/assets/javascripts/sephora_style_guide/carousel.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.product-card').click(function(e) {
-    if (e.target.className === 'carousel-arrow-right') {
+    if ($(e.target).hasClass('carousel-arrow-right')) {
       $('.product-card-variants').animate({ scrollLeft: '+=60' }, 500);
       $('.carousel-more-variants').hide();
-    } else if (e.target.className === 'carousel-arrow-left') {
+    } else if ($(e.target).hasClass('carousel-arrow-left')) {
       $('.product-card-variants').animate({ scrollLeft: '-=60' }, 500);
       $('.carousel-more-variants').show();
     }
