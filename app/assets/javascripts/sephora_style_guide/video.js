11209892c78cbf5a02da86e03e8c29dfe6bcc651
Eric Ewe Yow Choong <eric.ewe@luxola.com>
2018-08-16 16:49:56 +0800

    [CFE-525] Refactor to hound

--- app/assets/javascripts/sephora_style_guide/video.js
+++ app/assets/javascripts/sephora_style_guide/video.js
@@ -18,7 +18,7 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady() {
   // bind events
-  $(document).on('click', '.video-link', function(e) {
+  $(document).on('click', '.video-link', function() {
     var currentVideoId = $(this).attr('data-video');
 
     for (var i in players) {
@@ -28,7 +28,7 @@ function onPlayerReady() {
     }
   });
 
-  $('.modal[role="dialog"]').on('hide.bs.modal', function(e) {
+  $('.modal[role="dialog"]').on('hide.bs.modal', function() {
     var currentVideoId = $(this).find('iframe').attr('id');
 
     for (var i in players) {
