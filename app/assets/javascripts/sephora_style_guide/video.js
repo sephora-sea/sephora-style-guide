57b2ac34ecd42f1962e20eb59aeb945616afeded
Thanh Che <cqthanh@tma.com.vn>
2020-04-16 14:28:18 +0700

    [EC-478] Fixed cannot close Youtube video

--- app/assets/javascripts/sephora_style_guide/video.js
+++ app/assets/javascripts/sephora_style_guide/video.js
@@ -22,7 +22,7 @@ function onPlayerReady() {
     var currentVideoId = $(this).attr('data-video');
 
     for (var i in players) {
-      if (players[i].a.id === currentVideoId) {
+      if (players[i].f.id === currentVideoId) {
         players[i].playVideo();
       }
     }
@@ -32,7 +32,7 @@ function onPlayerReady() {
     var currentVideoId = $(this).find('iframe').attr('id');
 
     for (var i in players) {
-      if (players[i].a.id === currentVideoId) {
+      if (players[i].f.id === currentVideoId) {
         players[i].pauseVideo();
       }
     }
